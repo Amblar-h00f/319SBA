79677d6fc7a4da3e8adea1a1ca3a72fd30246076
Amblar-h00f <amberkollm3@gmail.com>
2025-04-28 18:27:16 -0600

    fifth

---
+++ routes/seashellRoutes.mjs
@@ -0,0 +1,47 @@
+import express from 'express';
+import Shell from '../models/seashellModel.mjs';
+
+const router = express.Router();
+
+/// create a new seashell
+router.post('/', async (req, res) => {
+
+    const newShell = await Shell.create(req.body);
+
+    //return results
+    res.json(newShell);
+});
+
+//read
+router.get('/', async (req, res) => {
+    const allShell = await Shell.find({});
+
+    //return results
+    res.json(allShell);
+});
+
+//Update
+router.put('/:id', async (req, res) => {
+
+    const editShell = await Shell.findByIDandUpdate
+    (req.params.id, req.body, { new: true, });
+});
+
+if (!editShell) res.status(400).json({ msg: 'Shell not found' });
+
+res.json(editShell);
+
+//delete
+
+router.delete('/:id', async (req, res) => { 
+    //Specify Action
+    const deleteShell = await Shell.findByIdAndDelete(req.params.id);
+
+    if (!deleteShell) res.status(400).json({ msg: 'Shell not found'});
+
+    res.json(deleteShell);
+});
+
+export default router;
+
+
